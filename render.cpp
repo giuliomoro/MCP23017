cb697edff31356ea1006d93fb4af8bb97d31f343
Giulio Moro <giuliomoro@yahoo.it>
2021-04-12 17:38:23 +0000

    Using 4 encoders

--- render.cpp
+++ render.cpp
@@ -3,12 +3,20 @@
 #include <libraries/Encoder/Encoder.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <thread>
+#include <vector>
 
-Encoder encoder(0, Encoder::ANY);
+std::vector<Encoder> encoders;
+std::vector<std::array<uint8_t, 2>> pinsPairs = {
+	{{0, 1}},
+	{{2, 3}},
+	{{4, 5}},
+	{{6, 7}},
+};
+
 uint16_t gpio;
 MCP23017 mcp(1, 0x22);
-uint8_t pins[2] = {0, 1};
 
 void makeBinaryString(char* str, uint16_t data)
 {
@@ -30,7 +38,12 @@ void processEnc()
 		// allows us to see which pin toggled first (useful for encoders!)
 		static uint16_t oldGpio;
 		gpio = mcp.readINTCAP(0);
-		encoder.process(gpio & (1 << pins[0]), gpio & (1 << pins[1]));
+		for(unsigned int n = 0; n < pinsPairs.size(); ++n)
+		{
+			auto& pins = pinsPairs[n];
+			Encoder& encoder = encoders[n];
+			encoder.process(gpio & (1 << pins[0]), gpio & (1 << pins[1]));
+		}
 		if(oldGpio != gpio)
 		{
 			// we caught the device as an interrupt had just occurred.
@@ -44,6 +57,8 @@ void processEnc()
 }
 // Input #0 is on pin 21 so connect a button or switch from there to ground
 int main() {
+	for(unsigned int n = 0; n < pinsPairs.size(); ++n)
+		encoders.push_back({0, Encoder::ANY});
 	std::thread th(processEnc);
 	if(!mcp.openI2C())
 	{
@@ -55,30 +70,44 @@ int main() {
 		mcp.pinMode(n, MCP23017::INPUT);
 		mcp.pullUp(n, MCP23017::HIGH);  // turn on a 100K pullup internally
 	}
-	int ss = 100;
+	int ss = 20;
 	char stars[ss + 1];
-	for(unsigned int n = 0; n < ss; ++n)
-	{
-		stars[n] = '*';
-	}
-	stars[ss] = '\0';
+	char spaces[ss + 1];
+	memset(stars, '*', ss);
+	memset(spaces, ' ', ss);
+	spaces[ss] = stars[ss] = '\0';
+	std::vector<int> oldRots(encoders.size());
+	uint16_t oldGpio = 0;
 	while(true){
-		static int oldRot = 0;
-		static uint16_t oldGpio = 0;
-		int rot = encoder.get();
-		if(gpio != oldGpio || oldRot != rot)
+		bool shouldPrint = false;
+		if(gpio != oldGpio)
+			shouldPrint = true;
+		oldGpio = gpio;
+		for(unsigned int n = 0; n < encoders.size(); ++n)
+		{
+			Encoder& encoder = encoders[n];
+			int& oldRot = oldRots[n];
+			int rot = encoder.get();
+			if(oldRot != rot)
+				shouldPrint = true;
+			oldRot = rot;
+		}
+		if(shouldPrint)
 		{
 			char str[17];
 			makeBinaryString(str, gpio);
-			int numStars = rot + 1;
-			if(numStars > ss)
-				numStars = ss;
-			else if (numStars < 0)
-				numStars = 0;
-			printf("%s: %d %.*s\n", str, rot, numStars, stars);
+			printf("%s: ", str);
+			for(auto& e : encoders)
+			{
+				int rot = e.get();
+				int numStars = rot + 1;
+				while(numStars < 0)
+					numStars += ss;
+				numStars %= ss;
+				printf("%4d %4d %.*s%.*s", rot, numStars, numStars, stars, ss - numStars, spaces);
+			}
+			printf("\n");
 		}
-		oldRot = rot;
-		oldGpio = gpio;
 		usleep(50000);
 	}
 	return 0;
