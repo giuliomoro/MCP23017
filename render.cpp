fb1b1d18b33b298354d17f4cb4be821d713f6783
Giulio Moro <giuliomoro@yahoo.it>
2021-04-14 12:26:12 +0000

    Using multiple encoder boards

--- render.cpp
+++ render.cpp
@@ -13,6 +13,7 @@
 std::vector<Oscillator> oscillators;
 std::vector<OnePole> smoothers;
 std::vector<Encoder> encoders;
+const unsigned int kEncodersPerBoard = 4;
 std::vector<std::array<uint8_t, 2>> pinsPairs = {
 	{{6, 5}},
 	{{3, 2}},
@@ -20,8 +21,17 @@ std::vector<std::array<uint8_t, 2>> pinsPairs = {
 	{{12, 13}},
 };
 
-uint16_t gpio;
-MCP23017 mcp(1, 0x22);
+int i2cBus = 1;
+std::vector<uint8_t> i2cAddresses = {
+	0x21,
+	0x22,
+	0x26,
+	0x27,
+};
+
+std::vector<uint16_t> oldGpios;
+std::vector<uint16_t> gpios;
+std::vector<MCP23017> mcps;
 
 void makeBinaryString(char* str, uint16_t data)
 {
@@ -32,30 +42,33 @@ void makeBinaryString(char* str, uint16_t data)
 
 void processEnc(void*)
 {
-	// we set up interrupts but don't actually use the interrupt pins.
-	mcp.setupInterrupts(true, false, MCP23017::HIGH);
-	for(unsigned int n = 0; n < 16; ++n)
-		mcp.setupInterruptPin(n, MCP23017::CHANGE);
+	std::vector<uint16_t> oldGpios = gpios;
 	while(!gShouldStop)
 	{
-		// we always read from INTCAP (the state of the GPIO when the
-		// interrupt was triggered). This resets the interrupt and
-		// allows us to see which pin toggled first (useful for encoders!)
-		static uint16_t oldGpio;
-		gpio = mcp.readINTCAPAB();
-		for(unsigned int n = 0; n < pinsPairs.size(); ++n)
+		for(unsigned int m = 0; m < mcps.size(); ++m)
 		{
-			auto& pins = pinsPairs[n];
-			encoders[n].process(gpio & (1 << pins[0]), gpio & (1 << pins[1]));
-		}
-		if(oldGpio != gpio)
-		{
-			// we caught the device as an interrupt had just occurred.
-			// add a little "debouncing" delay before reading again
-			usleep(500);
-			// read and ignore in order to clear any interrupt that
-			// may have come up in the meantime.
-			mcp.readINTCAP(0);
+			MCP23017& mcp = mcps[m];
+			uint16_t& oldGpio = oldGpios[m];
+			uint16_t& gpio = gpios[m];
+			// we always read from INTCAP (the state of the GPIO when the
+			// interrupt was triggered). This resets the interrupt and
+			// allows us to see which pin toggled first (useful for encoders!)
+			gpio = mcp.readINTCAPAB();
+			for(unsigned int n = 0; n < pinsPairs.size(); ++n)
+			{
+				auto& pins = pinsPairs[n % kEncodersPerBoard];
+				encoders[n + m * kEncodersPerBoard].process(gpio & (1 << pins[0]), gpio & (1 << pins[1]));
+			}
+			if(oldGpio != gpio)
+			{
+				// we caught the device as an interrupt had just occurred.
+				// add a little "debouncing" delay before reading again
+				// TODO: do not hold back reading back other devices while debouncing one device
+				usleep(500);
+				// read and ignore in order to clear any interrupt due to bounces that
+				// may have occurred in the meantime.
+				mcp.readINTCAP(0);
+			}
 		}
 	}
 }
@@ -69,57 +82,83 @@ void printEnc(void*)
 	memset(spaces, ' ', ss);
 	spaces[ss] = stars[ss] = '\0';
 	std::vector<int> oldRots(encoders.size());
-	uint16_t oldGpio = 0;
+	std::vector<uint16_t> oldGpios = gpios;
 	while(!gShouldStop){
-		bool shouldPrint = false;
-		if(gpio != oldGpio)
-			shouldPrint = true;
-		oldGpio = gpio;
-		for(unsigned int n = 0; n < encoders.size(); ++n)
+		for(unsigned int m = 0; m < mcps.size(); ++m)
 		{
-			int& oldRot = oldRots[n];
-			int rot = encoders[n].get();
-			if(oldRot != rot)
+			uint16_t& gpio = gpios[m];
+			uint16_t& oldGpio = oldGpios[m];
+			bool shouldPrint = false;
+			if(gpio != oldGpio)
 				shouldPrint = true;
-			oldRot = rot;
-		}
-		if(shouldPrint)
-		{
-			char str[17];
-			makeBinaryString(str, gpio);
-			printf("%s: ", str);
-			for(auto& e : encoders)
+			oldGpio = gpio;
+			unsigned int encStart = m * kEncodersPerBoard;
+			unsigned int encEnd = (1 + m) * kEncodersPerBoard;
+			for(unsigned int n = encStart; n < encEnd; ++n)
 			{
-				int rot = e.get();
-				int numStars = rot + 1;
-				while(numStars < 0)
-					numStars += ss;
-				numStars %= ss;
-				printf("%4d %.*s%.*s", rot, numStars, stars, ss - numStars, spaces);
+				int& oldRot = oldRots[n];
+				int rot = encoders[n].get();
+				if(oldRot != rot)
+					shouldPrint = true;
+				oldRot = rot;
 			}
-			printf("\n");
+			if(shouldPrint)
+			{
+				char str[17];
+				makeBinaryString(str, gpio);
+				printf("[%d] %s: ", m, str);
+				for(unsigned int n = encStart; n < encEnd; ++n)
+				{
+					int rot = encoders[n].get();
+					int numStars = rot + 1;
+					while(numStars < 0)
+						numStars += ss;
+					numStars %= ss;
+					printf("%4d %.*s%.*s", rot, numStars, stars, ss - numStars, spaces);
+				}
+				printf("\n");
+			}
+			usleep(50000);
 		}
-		usleep(50000);
 	}
 }
 
 bool setup(BelaContext* context, void*)
 {
-	if(!mcp.openI2C())
+	mcps.reserve(i2cAddresses.size()); // ensure no allocation happens in the below loop.
+	for(unsigned int c = 0; c < i2cAddresses.size(); ++c)
 	{
-		fprintf(stderr, "Failed to open device\n");
-		return false;
+		mcps.emplace_back(i2cBus, i2cAddresses[c]);
+		if(!mcps.back().openI2C())
+		{
+			fprintf(stderr, "Failed to open device on bus %d, address %#x\n", i2cBus, i2cAddresses[c]);
+			mcps.erase(mcps.end() - 1, mcps.end());
+			continue;
+		}
+		MCP23017& mcp = mcps.back();
+		gpios.push_back(0);
+		for(unsigned int n = 0; n < 16; ++n)
+		{
+			mcp.pinMode(n, MCP23017::INPUT);
+			mcp.pullUp(n, MCP23017::HIGH);  // turn on a 100K pullup internally
+			// we set up interrupts so we can read the INTCAP register
+			mcp.setupInterrupts(true, false, MCP23017::HIGH);
+			for(unsigned int n = 0; n < 16; ++n)
+				mcp.setupInterruptPin(n, MCP23017::CHANGE);
+		}
 	}
-	for(unsigned int n = 0; n < pinsPairs.size(); ++n)
+	if(!mcps.size())
+	{
+		fprintf(stderr, "No device detected\n");
+		return false;
+	} else {
+		printf("%d encoder boards detected\n", mcps.size());
+	}
+	for(unsigned int n = 0; n < mcps.size() * kEncodersPerBoard; ++n)
 	{
 		oscillators.push_back({context->audioSampleRate});
 		smoothers.push_back({context->audioSampleRate / context->audioFrames, context->audioSampleRate});
 		encoders.push_back({0, Encoder::ANY});
-	}
-	for(unsigned int n = 0; n < 16; ++n)
-	{
-		mcp.pinMode(n, MCP23017::INPUT);
-		mcp.pullUp(n, MCP23017::HIGH);  // turn on a 100K pullup internally
 	}
 	Bela_runAuxiliaryTask(processEnc, 90);
 	Bela_runAuxiliaryTask(printEnc, 0);
